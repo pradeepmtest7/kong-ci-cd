561ced9ef5f097e0817feae03867cbfb37c9518a
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2021-09-02 17:22:05 -0400

    Add it all

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const server = express();
+const port = 3000;
+server.get("/", (req, res) => {
+  res.status(200).send("Hello, world!");
+});
+server.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+node server.js &
+sleep 3 # give server time to boot
+
+stop_server() {
+  pgrep node | xargs kill
+}
+curl http://localhost:8000/api
+code="$?"
+
+if [ "$code" -ne "0" ]; then
+  stop_server
+  echo "FAIL CODE $code";
+  exit $code;
+fi
+
+stop_server
